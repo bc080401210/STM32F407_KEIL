c5a034b1d83055b5c6d9935390a5ee455e606cc5
bc080401210 <bc080401210@vu.edu.pk>
2016-09-24 18:51:10 +0500

    uart controlled files added
    uart project added

---
+++ uart_sw_led_ex01/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'uart_sw_led_ex01' 
+ * Target:  'uart_sw_led_ex01' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
